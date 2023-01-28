9566f105be49cd755f87f04bc1359aa8ad923857
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-01-27 22:58:08 -0300

    Border component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "../styles/Footer.module.css"
+import Border from "./Border";
 
 export default function Footer(){
     return(
         <>
         <div className={styles.general}>
+           <Border></Border> 
             <div className={styles.copy}>
                 <h5>Copyright © Doble v - 2023</h5>
                 <h5>Diseño web <a href="https://github.com/EugeniaGramajo/doble-v" target="_blank">E Gramajo - M Lujan</a> </h5>
