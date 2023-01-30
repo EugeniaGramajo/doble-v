c4999786e3505a7ce10d2d865449f82cd869596f
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-01-30 19:53:47 -0300

    header css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import styles from "../styles/Home.module.css"
+import styles from "../styles/Home.module.css";
 
-export default function Home(){
-    return(
-        <>
-        <div  className={styles.container}
-         data-aos="flip-down"
->
+export default function Home() {
+  return (
+    <>
+      <div className={styles.container} data-aos="flip-down">
+        <div className={styles.general}>
+            <h1> Se te antoja un tatuaje?</h1>
+        </div>
 
-        
-        <div  className={styles.general}
-        
-        >
-         
-        </div>
-        </div>
-         </>
-    )
+      </div>
+    </>
+  );
 }
