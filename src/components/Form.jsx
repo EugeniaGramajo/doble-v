0e643debcc9399019f9308d87baaac0cea73a2e3
EusebioLujan <narboleta@gmail.com>
2023-02-01 01:45:40 -0300

    fix-responsive-more-formsubmit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import { validate } from "../Data/validate";
 import styles from "../styles/Form.module.css"
 
 
@@ -12,48 +11,13 @@ export default function Form() {
         email: "",
         msg: "",
     });
-    const [errors, setErrors] = useState({
-        name: "",
-        asunto: "",
-        email: "",
-        msg: "",
-    });
     const handleChange = (event) => {
         setInputs({
             ...inputs,
             [event.target.name]: event.target.value
         });
-
-        setErrors(validate({
-            ...inputs,
-            [event.target.name]: event.target.value
-        }));
     }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (Object.keys(errors).length === 0) {
-            alert("Datos completos");
-            setInputs({
-                name: "",
-                asunto: "",
-                email: "",
-                msg: "",
-            });
-            setErrors({
-                name: "",
-                asunto: "",
-                email: "",
-                msg: "",
-            });
-        } else {
-            alert("Debe llenar todos los campos");
-        }
-
-    }
-
-
-
+    
 
 
     return (
@@ -61,25 +25,26 @@ export default function Form() {
 
 
             <div className={styles.divform}>
-                <form onSubmit={handleSubmit}>
+                <form action="https://formsubmit.co/gramajoeugenia@gmail.com" method="POST" >
 
                     <div className={styles.hh1}>
                         <h1 >Formulario de contacto</h1>
 
                     </div>
                     <div className={styles.divnamenum}>
+                    <input type="hidden" name="_next" value="http://localhost:3000/"></input>
+                        <input type="hidden" name="_captcha" value="false"></input>
+                        <input type="text" name="name" value={inputs.name} onChange={handleChange} placeholder="Tu Nombre..." required></input>
 
-                        <input type="text" name="name" value={inputs.name} onChange={handleChange} placeholder="Tu Nombre..."required></input>
+                        <input type="text" name="asunto" value={inputs.asunto} onChange={handleChange} placeholder="Tu Asunto..." ></input>
 
-                        <input type="text" name="asunto"  value={inputs.asunto}onChange={handleChange} placeholder="Tu Asunto..." ></input>
-
-                        <input type="email" name="email" value={inputs.email} onChange={handleChange} placeholder="Tu Email..." required></input>
+                        <input type="email" name="email" value={inputs.email} onChange={handleChange} placeholder="Tu Email..." required ></input>
 
                     </div>
 
 
-                    <textarea className={styles.divmsg} value={inputs.msg} type="text" name="msg" onChange={handleChange} placeholder="Escribe Un Mensaje..." required></textarea>
-                    <p>{errors.msg}</p>
+                    <textarea className={styles.divmsg} value={inputs.msg} type="text" name="msg" onChange={handleChange} placeholder="Escribe Un Mensaje..." ></textarea>
+                    
 
 
                     <div className={styles.divbutton}>
